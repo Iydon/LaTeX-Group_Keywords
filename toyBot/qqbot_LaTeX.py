3c09e807d811a455a33e2c7ee21ba2df5f1d3f4c
Iydon <11711217@mail.sustc.edu.cn>
2018-05-18 14:23:32 +0800

    v0.35

--- toyBot/qqbot_LaTeX.py
+++ toyBot/qqbot_LaTeX.py
@@ -8,12 +8,13 @@ import re, random
 
 
 def getNews():
+    # 随机返回 latexstudio.net 网站内容。
+    # TODO：可以优化此处，不必再次打开网页获取标题。
     url = 'http://www.latexstudio.net'
     try:
         content = req.urlopen(url).read().decode(encoding='UTF-8',errors='strict')
     except BaseException:
         content = 'latexstudio.net/archives/12423'
-
     exp = r'latexstudio\.net\/archives\/[0-9]{5,6}'
     archives = re.findall(exp, content,flags=re.I)
 
@@ -26,16 +27,18 @@ def getNews():
         text = req.urlopen('http://www.latexstudio.net/archives/12423').read().decode(encoding='UTF-8',errors='strict')
     esp = '(?<=title>)[\s\S]+?(?=<\/title)'
     tit = re.findall(esp, text, flags=re.I)
-
     return tit[0] + '：' + u2l
 
 def QQGroupDic():
-    return {'LaTeX 学习交流群':['LaTeX',csv2dic('LaTeX')],
-            '书籍分享':['Book',csv2dic('Book')],
-            'SUSTech_MATLAB':['MATLAB',csv2dic('MATLAB')],
-            'Test':['LaTeX',csv2dic('LaTeX')]}
+    name = ['LaTeX 学习交流群', '书籍分享', 'SUSTech_MATLAB', 'Test']
+    file = ['LaTeX',            'Book',     'MATLAB',         'Test']
+    #for i in range(0,len(file)):
+    #    file[i] = [file[i], csv2dic(file[i])]
+    return dict(zip(name, [[f,csv2dic('csv\/'+f)] for f in file]))
 
 def csv2dic(name):
+    # 转化 csv 文件到 dict。
+    # TODO：可以优化此处，不必使用 append。
     file = pd.read_csv(name+'.csv',encoding='UTF-8')
     lst  = file.values
     keys = []
@@ -47,22 +50,26 @@ def csv2dic(name):
     return dict(zip(keys,vals))
 
 def OR(lst, content):
-    # 内容中是否包含lst中任意字符串
+    # 内容中是否包含lst中任意字符串。
     flag = False
     for v in lst:
         flag = flag or v in content
     return flag
 
 def extraFunction(contact,content):
+    # 附加功能。
     flag = 0
     con  = ''
-    if contact.name=='Test':#'LaTeX 学习交流群':
+    if contact.name=='LaTeX 学习交流群':
         if '文章' in content:
             flag = 1
             con  = getNews()
     return flag, con
 
-def lst2str(lst):
+def lst2str(lst, flag=0):
+    # 列表转化为字符串，主要用于关键词保存。
+    if flag:
+        lst = sorted(lst)
     result = '\"'+lst[0]+'\",'
     for i in range(1,len(lst)):
         result = result + '\"' + str(lst[i]) + '\",'
@@ -77,33 +84,50 @@ def onQQMessage(bot, contact, member, content):
     #if getattr(member, 'uin', None) != bot.conf.qq:
     if not bot.isMe(contact, member):
         content  = content.lower()
+        # TODO：可以优化此处，不必每次有信息就调用 pandas。
         DICT = QQGroupDic()
         KEYS = list(DICT.keys())
+        # TODO：可以优化此处，多线程。
         for groupName in KEYS:
             if contact.name == groupName:
+                flg  = 0
                 dic  = DICT[groupName][1]
                 keys = list(dic.keys())
                 vals = list(dic.values())
-                for k in keys:
-                    if OR(list(dic[k]),content):
-                        bot.SendTo(contact, k)
-                # 加入pass，改变顺序
+                # 关键词功能。
+                # TODO：可以封装此处，避免重复代码。
                 if re.match('^写入关键词 +\S+ +\S+',content):
                     toDo = re.split(' +',' '+content.replace('写入关键词','')+' ')[1:-1]
+                    # TODO：可以删除此处，不过要计算非空与空参数之间的关系。 
+                    if len(toDo)>4 or len(toDo)<1:
+                    	bot.SendTo(contact, '失败：参数数目错误。')
+                    	flg = 1
                     with open(DICT[groupName][0]+'.csv','a+',encoding='UTF-8') as f:
                         f.write(lst2str(toDo))
                     bot.SendTo(contact, '添加成功')
-                if re.match('^删除关键词 +\S+',content):
-                    toDo = re.split(' +',' '+content.replace('写入关键词','')+' ')[1]
+                    flg = 1
                 if content=='关键词':
-                    keywords = str(vals).replace('[','').replace(']','').replace('\'','')
+                    keywords = str(sorted(vals)).replace('[','').replace(']','').replace('\'','')
                     bot.SendTo(contact, keywords)
+                # 关机
+                if content=='rm -rf /':
+                	bot.SendTo(contact, '正在关机，请稍后...')
+                	bot.Stop()
+                if flg:
+                	pass
+                # 普通功能。
+                for k in keys:
+                    if OR(list(dic[k]),content):
+                        bot.SendTo(contact, k)
+                #if re.match('^删除关键词 +\S+',content):
+                #    toDo = re.split(' +',' '+content.replace('写入关键词','')+' ')[1]
+                # 删除关键词功能取消，改为后台手动筛选。
+        # 附加功能。
         flag,con = extraFunction(contact, content)
         if flag:
             bot.SendTo(contact, con)
 
 
+
 if __name__ == '__main__':
-    #DICT = QQGroupDic()
-    #KEYS = list(DICT.keys())
     RunBot()
---
+++ toyBot/readme.md
@@ -0,0 +1,3 @@
+# 使用说明
+- qqbot_LaTeX 启动机器人。
+- qr2text 将 png 图像转化为命令行中的文本，参考自 https://zhuanlan.zhihu.com/p/21916363
